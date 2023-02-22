e16768a630aa4447abfd637c49e1312c66a79bf3
Jesus Juvinao <juvinaojesusd@gmail.com>
2023-02-22 18:01:19 -0500

    fea

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 // Add Express
 const express = require("express");
-
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 // Initialize Express
 const app = express();
-
+app.use(cors());
+app.use(express.json());
 // Create GET request
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+    // console.log(req)
+    const username = req.body.username;
+    const password = req.body.password;
+    if (!password) return res.status(200).json({
+        success: false,
+        message: 'Error en la contraseña'
+      })
+    if (password) {
+        const secretKey = "mi_clave_secreta";
+        const token = jwt.sign({ username: username }, secretKey);
+        console.log(token)
+        res.send(`Hola  ${token}`);
+        return token
+    }
+    res.send(`Hola papu ${username || ''}`);
+});
+
+app.post("/", (req, res) => {
+        // console.log(req)
+        const username = req.body.username;
+        const password = req.body.password;
+        if (!password) return res.status(200).json({
+            success: false,
+            message: 'Error en la contraseña'
+        })
+        if (password) {
+            const secretKey = "mi_clave_secreta";
+            const token = jwt.sign({ username: username }, secretKey);
+            console.log(token)
+            res.send(`Hola  ${token}`);
+            return token
+        }
+        res.send(`Hola papu ${username || ''}`);
 });
 
 // Initialize server
 app.listen(5000, () => {
-  console.log("Running on port 5000.");
+    console.log("Running on port 5000.");
 });
 
 // Export the Express API
